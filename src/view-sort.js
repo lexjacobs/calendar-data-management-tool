72dddebcac23a8b8f41adaa558e61928842d258a
Alex Jacobs <lex.jacobs@gmail.com>
2017-07-17 10:44:50 -0700

    Append calendar items to page, adding shading if any present.

--- src/view-sort.js
+++ src/view-sort.js
@@ -2,11 +2,13 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 import moment from 'moment';
 import dailyViews from './dailyViews'
+import './css-sort.css';
 
 export const SortView = Backbone.View.extend({
   initialize() {
     this.render();
   },
+  tagName: 'div',
   render() {
 
     this.$el.append(`Sorted View<br>`)
@@ -14,13 +16,41 @@ export const SortView = Backbone.View.extend({
     var START = moment("2016-08-30");
     var END = moment("2016-09-23");
 
-    dailyViews(START, END).models.forEach(x => {
-      this.$el.append(`${x.get('date').calendar()} <br>`);
+    var sortedDailyViews = dailyViews(START, END);
+
+    sortedDailyViews.models.forEach(x => {
+      this.$el.append(`<br>${x.get('date').calendar()} <br>`);
+
+      this.$el.append(new ItemView({
+        collection: new Backbone.Collection(x.get('events'))
+      }).el)
+
       x.get('events').forEach(x => {
-        this.$el.append(`${_.values(_.omit(x.attributes, ['occurrences', 'repeat', 'shading', 'mlh', 'asp']))}<br>`);
+        console.log(_.values(_.omit(x.attributes, ['occurrences', 'repeat', 'shading', 'mlh', 'asp'])));
       })
     })
 
+    return this;
+  }
+});
+
+const ItemView = Backbone.View.extend({
+  initialize(options) {
+
+    // check for any instance of shading
+    if (options.collection.models.filter(x => {
+        return x.get('shading') !== undefined;
+      }).length) this.$el.addClass('shading');
+
+    this.render();
+  },
+  className: 'items',
+  tagName: 'div',
+  template: _.template('<%= text %><br>'),
+  render() {
+    this.collection.models.forEach(x => {
+      this.$el.append(this.template(x.attributes));
+    })
     return this;
   }
 });
