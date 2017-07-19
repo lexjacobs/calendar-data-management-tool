5f97e2343783beffd99c7d8bd2c90e26e308a544
Alex Jacobs <lex.jacobs@gmail.com>
2017-07-19 13:16:54 -0700

    Implement filtering of events view

--- src/view-events.js
+++ src/view-events.js
@@ -1,5 +1,5 @@
 import Backbone from 'backbone';
-// import $ from 'jquery';
+import $ from 'jquery';
 // import _ from 'underscore';
 // import moment from 'moment';
 import './css-events.css';
@@ -7,25 +7,43 @@ import database from './collection-database';
 
 export const EventsView = Backbone.View.extend({
   initialize() {
-    this.collection = database;
-    this.listenTo(this.collection, 'updated', this.render);
+    this.filterChooser = new FilterChooser();
+    this.individualEventBlock = new IndividualEventBlock();
     this.render();
+    this.listenTo(this.filterChooser, 'filterClicked', this.filterEvents);
+  },
+  filterEvents(e) {
+    this.individualEventBlock.trigger('drawWithFilter', e)
   },
   render() {
     this.$el.html('');
-    this.$el.append(new IndividualEventBlock().el);
+    this.$el.append(this.filterChooser.el);
+    this.$el.append(this.individualEventBlock.el);
     return this;
   }
 });
 
 const IndividualEventBlock = Backbone.View.extend({
   initialize() {
+    this.model = new Backbone.Model({
+      filter: {}
+    });
     this.collection = database;
+    this.listenTo(this.collection, 'updated', this.render);
+    this.listenTo(this, 'drawWithFilter', this.drawWithFilter)
+    this.listenTo(this.model, 'change', this.render);
     this.render();
+  },
+  drawWithFilter(e) {
+    if (e === 'all') {
+      this.model.set({'filter': {}})
+    } else {
+      this.model.set({'filter': {repeat: e}});
+    }
   },
   render() {
     this.$el.html('');
-    this.collection.models.forEach(function(x) {
+    this.collection.where(this.model.get('filter')).forEach(function(x) {
       this.$el.append(new IndividualEvent({
         model: x
       }).el);
@@ -37,14 +55,32 @@ const IndividualEventBlock = Backbone.View.extend({
 const IndividualEvent = Backbone.View.extend({
   initialize() {
     this.render();
+    this.collection = database;
   },
   events: {click: 'answer'},
   answer(e) {
-    console.log('heard it', e.target);
-    console.log(this.model.attributes);
+    console.log('heard click', this.model.cid, this.model.attributes);
   },
   render() {
-    this.$el.html(`${this.model.get('text')}<br>`);
+    this.$el.html(`${this.model.get('text')}`);
     return this;
   }
 })
+
+const FilterChooser = Backbone.View.extend({
+  initialize() {
+    this.render();
+  },
+  className: 'filter-chooser well',
+  events: {
+    click: 'clickHandler'
+  },
+  clickHandler(e) {
+    let clickTarget = $(e.target).data('repeat');
+    this.trigger('filterClicked', clickTarget);
+  },
+  render() {
+    this.$el.html(`Filter by: <span data-repeat="annual">Annual events</span> | <span data-repeat="once">Changing Events</span> | <span data-repeat="banner">Month headings</span> | <span data-repeat="all">none</span>`);
+    return this;
+  }
+});
