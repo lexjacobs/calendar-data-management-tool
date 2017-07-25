ee405c3ef9ef3c087197f9e81e6b1d782e9a6655
Alex Jacobs <lex.jacobs@gmail.com>
2017-07-25 15:31:54 -0700

    Remove test routines from collection-database.js

--- src/collection-database.js
+++ src/collection-database.js
@@ -1,48 +1,8 @@
 import Backbone from 'backbone';
 
-// uncomment to repopulate firebase from data set
-// import dataset from './dataset';
-
 import eventModel from './model-event';
 import { Firebase } from './firebase';
 var events = Firebase.database().ref('events');
-
-// test: add a new event
-// test: change the event timing
-
-// var test = new eventModel({
-//   text: 'test event banner repeat',
-//   timing: ['10'],
-//   repeat: 'banner',
-//   shading: 'diagonal',
-//   mlh: 'yes',
-//   proclamation: 'yes',
-//   previousSundown: 'yes',
-//   asp: 'yes'
-// });
-//
-// setTimeout(function () {
-//   console.log('timout 1');
-//   database.add(test);
-// }, 3000);
-//
-//
-// setTimeout(function () {
-//   console.log('timout 2');
-// // database.last().set('text', 'event model sample text changed')
-//   database.last().addNewTiming(['9']);
-//   database.last().addNewTiming(['11']);
-// }, 5000);
-//
-//
-// setTimeout(_ => {
-//   console.log('timout 3');
-//   database.pop();
-//
-// }, 10000);
-
-// end tests
-
 
 const Database = Backbone.Collection.extend({
   initialize() {
