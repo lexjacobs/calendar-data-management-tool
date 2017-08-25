8019398b156792fd974cb1a1a424006375c446a5
Alex Jacobs <lex.jacobs@gmail.com>
2017-08-25 19:07:10 -0400

    Improved syntax using apply

--- src/shared.js
+++ src/shared.js
@@ -1,15 +1,11 @@
-export function serializedAttributes(array, key) {
-  let result = {};
-  array.forEach(x => {
-    result[x.name] = x.value;
-  });
-  return result[key];
-}
-
 export function serializedObject(array) {
   let result = {};
   array.forEach(x => {
     result[x.name] = x.value;
   });
   return result;
+}
+
+export function serializedAttributes(array, key) {
+  return serializedObject.call(this, array)[key];
 }
